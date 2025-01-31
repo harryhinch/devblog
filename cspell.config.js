f35e5d0c6054e675f0d06d6ca370cf47d729983b
Harry <harry@hinch.dev>
2025-01-30 18:44:23 -0700

    added favicon, spellcheck plugin for dev, fixed spelling in selfhostedgit, fixed css

---
+++ cspell.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+	version: '0.2',
+	language: 'en',
+
+    words: [
+        'eleventy',
+        '11ty',
+        'kairos',
+        'mavica',
+        'Chronos',
+        'Kairos',
+        'gcode',
+        'Adafruit',
+        'autoplay',
+        'qwiic',
+        'oled',
+        'SAMD',
+        'backronym',
+        'hardpoint',
+        'hardpoints',
+        'PLA',
+        'PETG',
+        'powerbank',
+        'microcontroller',
+        'microcontrollers',
+        'gitea',
+        'tailscale',
+        'userspace',
+        'homelab',
+    ],
+    flagWords: [],
+
+}
